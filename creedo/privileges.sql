c49ad13b7cf63a238102591110a384bb701e48fc
Janis Kalofolias <kalofoli@ceid.upatras.gr>
2019-04-16 02:30:27 +0200

    Minis

--- creedo/privileges.sql
+++ creedo/privileges.sql
@@ -1,5 +1,5 @@
 CREATE USER 'creedo'@'localhost' IDENTIFIED BY 'creedo';
-GRANT ALL PRIVILEGES ON creedo.* TO 'creedo'@'localhost';
+GRANT ALL PRIVILEGES ON `creedo`.* TO 'creedo'@'localhost';
 FLUSH PRIVILEGES;
 
 
